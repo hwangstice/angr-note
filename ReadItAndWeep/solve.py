276631d1e80bd7543a10885fd3ebc0974f64d520
dathwang <datquochoang0587@gmail.com>
2025-04-22 23:48:25 +0700

    update ReadItAndWeel solution

--- ReadItAndWeep/solve.py
+++ ReadItAndWeep/solve.py
@@ -1,8 +1,6 @@
-import angr, claripy
-import logging
-from string import printable
+import angr, logging, claripy, string
 
-logging.getLogger('angr').setLevel(logging.INFO)
+logging.getLogger("angr").setLevel(logging.INFO)
 
 def hook(l=None):
     if l:
@@ -11,31 +9,33 @@ def hook(l=None):
     IPython.embed(banner1='', exit_msg='', confirm_exit=False)
     exit(0)
 
-p = angr.Project("./read_it")
+p = angr.Project('./read_it')
 
-USER_DATA_LENGTH = 36
+# Set state for user data
+USER_DATA_LENGTH = 32
+user_data = claripy.BVS("user_data", USER_DATA_LENGTH * 8)
 
-user_data = claripy.BVS('user_data', USER_DATA_LENGTH*8)
-s = p.factory.entry_state(stdin=user_data)
+# state start when user input data
+s = p.factory.entry_state(stdin=user_data) 
+
+# Ensure input are printable
 for i in range(USER_DATA_LENGTH):
     s.solver.add(
         claripy.Or(*(
             user_data.get_byte(i) == x 
-            for x in printable.encode('utf-8')  
+            for x in string.printable.encode('utf-8')  
         ))
     )
 
-sm = p.factory.simgr(s, veritesting=True)
+sm = p.factory.simulation_manager(s, veritesting=True) # veritesting for optimization
 
-# Find function address "read_and_print_flag"
-print_flag = p.loader.find_symbol("read_and_print_flag").rebased_addr
-sm.explore(find=print_flag, avoid=0x00C24)
+# Find addr of read_and_print_flag function
+print_flag_addr = p.loader.find_symbol("read_and_print_flag").rebased_addr
+sm.explore(find=print_flag_addr, avoid=0x400C24)
 
-if not sm.found[0]:
+# Check if not found
+if not sm.found:
     print("NOT FOUND")
     hook(locals())
 
-print(sm.found[0].posix.stdin.concretize())
-
-# Run hook
-hook(locals())
+print(sm.found[0].posix.dumps(0))
